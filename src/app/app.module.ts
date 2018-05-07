4f6416f2fddc05e9ba9140af99808fa99af3d743
Remy Nickley <jhmun216@gmail.com>
2018-05-08 07:56:30 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule, XHRBackend, RequestOptions } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
 
+import { routing } from './routing/app.routing';
+
+import { ClFooterComponent } from './components/client/cl-footer/cl-footer.component';
+import { ClHeaderComponent } from './components/client/cl-header/cl-header.component';
+import { ClHomeComponent } from './components/client/cl-home/cl-home.component';
+import { ClContainerComponent } from './components/client/cl-container/cl-container.component';
+import { ClWalletComponent } from './components/client/cl-wallet/cl-wallet.component';
+import { ClProfileComponent } from './components/client/cl-profile/cl-profile.component';
+import { ClLoginRegComponent } from './components/client/cl-login-reg/cl-login-reg.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    ClContainerComponent,
+    ClFooterComponent,
+    ClHeaderComponent,
+    ClHomeComponent,
+    ClWalletComponent,
+    ClProfileComponent,
+    ClLoginRegComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/client/cl-home/cl-home.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+declare var jquery: any;
+declare var $: any;
+
+@Component({
+  selector: 'app-cl-home',
+  templateUrl: './cl-home.component.html',
+  styleUrls: ['./cl-home.component.css']
+})
+export class ClHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log('aa');
+  }
+
+  chooseOne()
+  {
+    $('#ChoiceModal').modal('show');
+  }
+
+  openVoucher()
+  {
+    $('#ChoiceModal').modal('hide');
+    $('#VoucherModal').modal('show');
+  }
+
+  openClaim() {
+    $('#ChoiceModal').modal('hide');
+    $('#ClaimModal').modal('show');
+  }
+
+}
